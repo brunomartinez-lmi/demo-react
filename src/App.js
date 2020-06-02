6a81fc1f404690680e6d312f37c6860f52eae56f
Bruno Martinez <bruno.martinez@logmein.com>
2020-06-02 17:14:17 -0400

    Send one message

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Message from './components/Message/Message'
 import WriteControl from './components/WriteControl/WriteControl'
 
 const App = () => {
 
-  const sendHandler = (message) => {
-    alert(message)
+  const [message, setMessage] = useState({author: "Me", time: "10:10:01", text: ""})
+
+  const sendHandler = (text) => {
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    setMessage({
+      author: "Me",
+      time: time,
+      text: text
+    })
   }
 
   return (
+
     <div className="App">
       <WriteControl send={sendHandler} />
       <Message author="Bruno" time="10:00:23">Hello World</Message>
       <Message author="Toto" time="10:05:23">Hi !</Message>
-      <Message author="Titi" time="10:10:23">This is a very cool message</Message>
+      <Message author={message.author} time={message.time}>{message.text}</Message>
     </div>
   );
 }
